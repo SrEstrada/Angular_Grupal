15004b811b24df2f69086f2e60864471b24c1d94
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-28 23:48:19 -0500

    Comfigurando module routing en la app

--- science-quiz/src/app/app-routing/app-routing-module.ts
+++ science-quiz/src/app/app-routing/app-routing-module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './question/question.component';
+import { ResultsComponent } from './results/results.component';
 
-
+const routes: Routes = [
+ { path: '', component: HomeComponent },
+ { path: 'question/:id', component: QuestionComponent },
+ { path: 'results', component: ResultsComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
