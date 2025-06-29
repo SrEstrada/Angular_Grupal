5f4436f6fcd095e039d1df97aec3d768abb034be
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-29 18:51:02 -0500

    haciendo la logica del juego en los componentes

--- science-quiz/src/app/app.routes.ts
+++ science-quiz/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Question } from './question/question';
+import { Results } from './results/results';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home
+        
+    },
+    {
+        path: 'question/:id', 
+        component: Question
+    },
+    {
+        path: 'results', 
+        component: Results
     }
 ];
