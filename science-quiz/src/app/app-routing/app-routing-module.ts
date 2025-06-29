1dcfd79e61370a34ecc5994acffc6647e1fd9423
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-29 00:18:17 -0500

    agregando archivos faltantes para poder lanzar el app, por ahora todos los archivos estan sin personalizacion, solo se lanzo el app

--- science-quiz/src/app/app-routing/app-routing-module.ts
+++ science-quiz/src/app/app-routing/app-routing-module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { QuestionComponent } from './question/question.component';
-import { ResultsComponent } from './results/results.component';
+import { HomeComponent } from '../home/home.component';
+import { QuestionComponent } from '../question/question.component';
+import { ResultsComponent } from '../results/results.component';
 
 const routes: Routes = [
  { path: '', component: HomeComponent },
