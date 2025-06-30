8947085e73d212143ce32fa9d6acd113487a5070
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-29 22:30:14 -0500

    Agregando la función que elige una palabra al azar

--- ahorcado-game/src/app/word.ts
+++ ahorcado-game/src/app/word.ts
@@ -3,7 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class Word {
+export class WordService {
+  private words = ['ANGULAR', 'PROGRAMACION', 'COMPONENTE', 'SERVICIO', 'MODULO', 'JAVASCRIPT', 'VARIABLE'];
 
-  constructor() { }
+  getRandomWord(): string {
+    const index = Math.floor(Math.random() * this.words.length);
+    return this.words[index];
+  }
 }
