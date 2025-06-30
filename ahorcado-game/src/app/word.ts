47406809dd8dfdcd129da29e5753eba08fa2bf43
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-29 22:51:51 -0500

    corrigiendo el error de no eliminar gitignore, ademas de agregar el enrutamiento de la página

--- ahorcado-game/src/app/word.ts
+++ ahorcado-game/src/app/word.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class WordService {
+export class Word {
   private words = ['ANGULAR', 'PROGRAMACION', 'COMPONENTE', 'SERVICIO', 'MODULO', 'JAVASCRIPT', 'VARIABLE'];
 
-  getRandomWord(): string {
-    const index = Math.floor(Math.random() * this.words.length);
-    return this.words[index];
-  }
+    getRandomWord(): string {
+      const index = Math.floor(Math.random() * this.words.length);
+      return this.words[index];
+    }
 }
